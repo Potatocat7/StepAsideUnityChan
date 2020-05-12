8171c9f99001bc625265918b4c12e7e4c72a5abe
Potatocat7 <potatocat7@gmail.com>
2020-05-12 16:00:59 +0900

    lesson6 課題　再提出修正 各オブジェクトにつけたスクリプトからDestroy()を削除（それのみのスクリプトはスクリプト事態を削除） オブジェクト消去用のスクリプトを追加し各障害物用プレハブにセット

--- Assets/ObjDestoryContoller.cs
+++ Assets/ObjDestoryContoller.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrafficConeController : MonoBehaviour {
-    //コーンが画面外に出るときのUnitychanとのｚ距離
+public class ObjDestoryContoller : MonoBehaviour {
+    //オブジェクトが画面外に出るときのUnitychanとのｚ距離
     private float visiblePosZ = -7.0f;
 
     //Unitychanのオブジェクト読み込みよう
     private GameObject UnitychanObj;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         //Unitychanのオブジェクト情報所得
         this.UnitychanObj = GameObject.Find("unitychan");
 
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         //画面外に来たらこのオブジェクトをデストロイ
         if (this.UnitychanObj.transform.position.z > this.transform.position.z - this.visiblePosZ)
         {
